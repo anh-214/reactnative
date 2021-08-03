56efd3bc7849a2ef74f27284a55a6d6803a041f9
Bơ PTT <tuananh1x2@gmail.com>
2021-08-03 16:37:19 +0700

    update

--- App.js
+++ App.js
@@ -2,28 +2,49 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, Dimensions, StatusBar, ScrollView, RefreshControl, FlatList, SectionList } from 'react-native';
 
 export default function App() {
+  const [name,setName] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+  function onClickHandler (){
+    setSubmitted(!submitted)
+  }
   return (
-   <View>
-     <Text>123</Text>
-   </View>
+    <View style={styles.container}>
+      <Text style={styles.text}>Nhập tên của bạn:</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Nhập ở đây"
+        onChangeText={(value)=>setName(value)}
+      />
+      <Button
+        title= {submitted ? 'Submit' : 'Clear'}
+        onPress={onClickHandler}
+      />
+      {!submitted ? 
+      <Text style={styles.text}>Tên của bạn là: {name}</Text>
+      :
+      null
+      }
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: '#fff',
-    marginTop: StatusBar.currentHeight
-  },
-  item:{
-    flex:1,
-    backgroundColor: 'pink',
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: StatusBar.currentHeight,
+    alignItems: 'center'
   },
   text:{
     fontSize: 30,
     color: 'black'
+  },
+  textInput:{
+    borderWidth:1,
+    margin:10,
+    padding:10,
+    borderRadius:20,
+    width: '50%',
+    textAlign: 'center'
   }
 });
